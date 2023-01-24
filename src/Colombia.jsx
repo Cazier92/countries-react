2b7224076dca2226a22acda40af9218914c55341
Cazier92 <bryce.cazier@gmail.com>
2023-01-24 10:06:51 -0800

    add Colombia

---
+++ src/Colombia.jsx
@@ -0,0 +1,13 @@
+const Colombia = (props) => {
+  return ( 
+    <>
+    <div>
+      <h1>Colombia</h1>
+      <div>Capital City: {props.capCity}</div>
+      <div>Languages: {props.languages}</div>
+    </div>
+    </>
+  );
+}
+
+export default Colombia;
--- src/CountriesOfSAmerica.jsx
+++ src/CountriesOfSAmerica.jsx
@@ -2,6 +2,7 @@ import Argentina from "./Argentina";
 import Bolivia from "./Bolivia";
 import Brazil from "./Brazil";
 import Chile from "./Chile";
+import Colombia from "./Colombia";
 
 const CountriesOfSAmerica = (props) => {
   return ( 
@@ -11,6 +12,7 @@ const CountriesOfSAmerica = (props) => {
       <Brazil languages={'Portuguese'} capCity={'Sao Paolo'} />
       <Bolivia languages={'Spanish'} capCity={'La Paz/Sucre'} />
       <Chile languages={'Spanish'} capCity={'Santiago'} />
+      <Colombia languages={'Spanish'} capCity={'Bogotá'} />
     </div>
     </>
   );
